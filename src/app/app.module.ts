602290f8c34090fd0ec9c8fd718d095a85cc9e35
Milos Jajac <milosjajac13@gmail.com>
2017-04-24 14:50:39 +0200

    add map page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { Map } from '../pages/map/map';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -15,9 +15,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    Map
   ],
   imports: [
     BrowserModule,
@@ -27,9 +27,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    Map
   ],
   providers: [
     StatusBar,
---
+++ src/pages/map/map.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Map } from './map';
+
+@NgModule({
+  declarations: [
+    Map,
+  ],
+  imports: [],
+  exports: [
+    Map
+  ]
+})
+export class MapModule {}
