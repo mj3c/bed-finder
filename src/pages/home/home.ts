13a7bc5a76c444837d33d4d3d2d68ebdda6f387a
Milos Jajac <milosjajac13@gmail.com>
2017-04-30 23:36:01 +0200

    add logo on home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +6,77 @@ import { NavController } from 'ionic-angular';
     templateUrl: 'home.html'
 })
 export class HomePage {
+    @ViewChild('logo')
+    private _logoEl: ElementRef;
 
     constructor(public navCtrl: NavController) {
 
+    }
+
+    ionViewDidLoad() {
+        this.drawLogo();
+    }
+
+    drawLogo() {
+        let context = this._logoEl.nativeElement.getContext('2d');
+
+        let blue: string = '#324A5E';
+        let red: string = '#FF5A60';
+        let yellow: string = '#FFD05B';
+        let white: string = '#FFFFFF';
+
+        // circle
+        context.fillStyle = red;
+        context.beginPath();
+        context.arc(75, 75, 75, 0, 2*Math.PI);
+        context.fill();
+
+        // lower wood part
+        context.fillStyle = blue;
+        context.beginPath();
+        context.moveTo(20, 80);
+        context.lineTo(20, 110);
+        context.lineTo(25, 110);
+        context.lineTo(30, 105);
+        context.lineTo(120, 105);
+        context.lineTo(125, 110);
+        context.lineTo(130, 110);
+        context.lineTo(130, 80);
+        context.fill();
+
+        // upper wood part
+        context.beginPath();
+        context.moveTo(35, 30);
+        context.lineTo(35, 60);
+        context.lineTo(115, 60);
+        context.lineTo(115, 30);
+        context.fill();
+
+        // left pillow
+        context.fillStyle = yellow;
+        context.beginPath();
+        context.moveTo(42, 60);
+        context.lineTo(65, 60);
+        context.lineTo(65, 50);
+        context.lineTo(42, 50);
+        context.fill();
+
+        // right pillow
+        context.beginPath();
+        context.moveTo(108, 60);
+        context.lineTo(85, 60);
+        context.lineTo(85, 50);
+        context.lineTo(108, 50);
+        context.fill();
+
+        // sheets
+        context.fillStyle = white;
+        context.beginPath();
+        context.moveTo(25, 80);
+        context.lineTo(125, 80);
+        context.lineTo(115, 60);
+        context.lineTo(35, 60);
+        context.fill();
     }
 
 }
